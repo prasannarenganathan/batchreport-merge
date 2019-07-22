7744edcc0334471247e38022fce87e380127de87
prasanna renganathan <prasanna.renganathan@gmail.com>
2019-07-22 23:12:23 +0530

    commmit

--- src/main/java/com/cnasurety/extagencyint/ivans/batches/merge/service/MergeServiceImpl.java
+++ src/main/java/com/cnasurety/extagencyint/ivans/batches/merge/service/MergeServiceImpl.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.cnasurety.extagencyint.ivans.batches.merge.reader.NotificationReader;
 import com.cnasurety.extagencyint.ivans.batches.merge.writer.NotificationWriter;
 
-@Component
+@Service
 public class MergeServiceImpl implements MergeService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     
